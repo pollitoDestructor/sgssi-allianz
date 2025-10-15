dd3afe4d50b183dfd83d3eab4a007ade7ce868cf
Alexander Diez <diezortuzaralexander@gmail.com>
2025-10-15 19:14:10 +0200

    index.php actualizado para las nuevas opciones

--- app/index.php
+++ app/index.php
@@ -1,16 +1,27 @@
 <html>
-	<head>
-		<title>
-			Ejemplo página web SGSSI
-		</title>
-	</head>
-	<body>
-<form action="login/login_form.php">
-	<input type="submit" value="Log In">
-	
-	
+<head>
+    <title>Allianz</title>
+</head>
+<body>
+
+<h2>Hola mundo :)</h2>
+
+<!-- Botón para iniciar sesión -->
+<form action="login/login_form.php" method="get">
+    <input type="submit" value="Iniciar sesión">
+</form>
+<br>
+
+<!-- Botón para registrarse -->
+<form action="register/register_form.php" method="get">
+    <input type="submit" value="Registrarse">
+</form>
+<br>
+
+<!-- Botón para ver catálogo -->
+<form action="catalogo/catalogo.php" method="get">
+    <input type="submit" value="Ver catálogo">
 </form>
 
-	</body>
+</body>
 </html>
-
