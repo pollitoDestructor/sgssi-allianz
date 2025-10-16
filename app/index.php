89a91637dad4c3ba2fe3adc1bde0119b80206d77
Alexander Diez <diezortuzaralexander@gmail.com>
2025-10-16 12:26:28 +0200

    Aritz y yo hemos hecho la interfaz

--- app/index.php
+++ app/index.php
@@ -1,11 +1,24 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <title>Allianz</title>
+    <meta charset="UTF-8">
+    
+    <title>Allianz Labubu</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-<h2>Hola mundo :)</h2>
-
+<br>
+</br>
+<div class="content">
+<h1>Allianz Labubu</h1>
+</div>
+<br>
+</br>
+<br>
+</br> </br>
+<div class="box">
+<br>
+</br>
 <!-- Botón para iniciar sesión -->
 <form action="login/login_form.php" method="get">
     <input type="submit" value="Iniciar sesión">
@@ -22,6 +35,33 @@
 <form action="catalogo/catalogo.php" method="get">
     <input type="submit" value="Ver catálogo">
 </form>
+</div>
+<!-- Footer con redes sociales -->
+</br>
+<br>
+</br>
+<br>
+</br>
+<br>
+</br>
+<footer class="footer">
+    <center>
+        <hr size="2" color="black">
+        <p>Contacto y redes sociales:</p>
+        <div class="social-icons">
+            <a href="https://twitter.com/zoomadrid">
+                <img src="img/twitter.png" width="50" height="50" alt="Twitter">
+            </a>
+            <a href="https://www.instagram.com/zoo_aquarium_madrid/">
+                <img src="img/insta.png" width="50" height="50" alt="Instagram">
+            </a>
+            <a href="https://www.tiktok.com/">
+                <img src="img/TikTok.png" width="50" height="50" alt="TikTok">
+            </a>
+        </div>
+    </center>
+</footer>
 
 </body>
 </html>
+
