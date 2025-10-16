56af7a991f102575739bab2f5eb3fa42a5500c10
pollitoDestructor <gabrielgupo05@gmail.com>
2025-10-16 12:08:46 +0200

    index.php redirecciones corregidas

--- app/index.php
+++ app/index.php
@@ -7,19 +7,19 @@
 <h2>Hola mundo :)</h2>
 
 <!-- Botón para iniciar sesión -->
-<form action="login/login_form.php" method="get">
+<form action="/login/" method="get">
     <input type="submit" value="Iniciar sesión">
 </form>
 <br>
 
 <!-- Botón para registrarse -->
-<form action="register/register_form.php" method="get">
+<form action="/register/" method="get">
     <input type="submit" value="Registrarse">
 </form>
 <br>
 
 <!-- Botón para ver catálogo -->
-<form action="catalogo/catalogo.php" method="get">
+<form action="/catalogo/" method="get">
     <input type="submit" value="Ver catálogo">
 </form>
 
