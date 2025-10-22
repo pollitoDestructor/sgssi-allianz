6d26a8521120007259ca7920e86e18aa9a06348f
Shaman Alonso <shaman.seguridad@gmail.com>
2025-10-22 17:14:20 +0200

    Register actualizado con el script

--- app/register/register_form.php
+++ app/register/register_form.php
@@ -63,54 +63,7 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <title>Allianz Labubu - Registro de usuario</title>
     <link rel="stylesheet" href="../css/style.css">
-    <script>
-        function validarFormulario() {
-            const nombre = document.getElementById("nombre").value.trim();
-            const apellidos = document.getElementById("apellidos").value.trim();
-            const dni = document.getElementById("dni").value.trim();
-            const fecha_nac = document.getElementById("fecha_nac").value;
-            const telefono = document.getElementById("telefono").value.trim();
-            const email = document.getElementById("email").value.trim();
-            const contra = document.getElementById("contra").value;
-		
-	    // ============= Todo esto para verificar o validar los campos =============
-            if (!nombre || !apellidos || !dni || !fecha_nac || !telefono || !email || !contra) {
-                alert("Por favor, completa todos los campos.");
-                return false;
-            }
-            
-            // Regex para DNI
-            const dniRegex = /^[0-9]{8}[A-Z]$/;
-            if (!dniRegex.test(dni)) {
-                alert("DNI inválido, formato incorrecto.");
-                return false;
-            } else {
-                let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
-                let dniNumeros = parseInt(dni.substring(0, dni.length - 1));
-                let posicion = dniNumeros % (cadena.length - 1);
-                if (dni[dni.length - 1].toLowerCase() != cadena[posicion].toLowerCase()) {
-                    alert("DNI inválido, la letra no coincide.");
-                    return false;
-                }
-            }
-            
-	    // Regex para email
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(email)) {
-                alert("Correo electrónico inválido.");
-                return false;
-            }
-            
-	    // Regex para telefono
-            const telefonoRegex = /^[0-9]{9}$/;
-            if (!telefonoRegex.test(telefono)) {
-                alert("Número de teléfono inválido (9 dígitos).");
-                return false;
-            }
-
-            return true;
-        }
-    </script>
+    <script src="../js/regex.js"></script>
 </head>
 <body>
 <br><br>
